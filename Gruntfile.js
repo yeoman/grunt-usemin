756695e56a5c6fe6b53f6565c0bc6d5d43ef1767
Sindre Sorhus <sindresorhus@gmail.com>
2012-12-08 21:38:59 +0100

    Fix gruntfile and add jshint rules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,22 @@
 'use strict';
-
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
-    test: {
-      files: ['test/**/*.js']
-    },
     jshint: {
-      all: [ 'Gruntfile.js', 'lib/*.js', 'tasks/*.js', 'test/**/*.js'],
       options: {
         jshintrc: '.jshintrc'
       },
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'default'
+      all: [
+        'Gruntfile.js',
+        'lib/*.js',
+        'tasks/*.js',
+        'test/**/*.js'
+      ]
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  // Load local tasks.
   grunt.loadTasks('tasks');
 
-  // Default task.
-  grunt.registerTask('default', 'jshint');
-
+  grunt.registerTask('default', ['jshint']);
 };
