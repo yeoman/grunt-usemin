6c6c24e64dc859ccad15b367352cb5786e3aa6fb
Frederick Ros <frederick.ros@gmail.com>
2013-02-21 14:48:12 +0100

    Regexp quote the filename before looking on disk

--- test/test-revvedfinder.js
+++ test/test-revvedfinder.js
@@ -36,6 +36,15 @@ describe('RevvedFinder', function () {
       assert.equal('bar/2345.image.png', rf.find('bar/image.png', '.'));
     });
 
+    it('should regexp quote the looked-after file', function(done) {
+      var rf = new RevvedFinder(function (pattern) {
+        assert.equal('**/*image\\.png', pattern);
+        done();
+        return[];
+      });
+      rf.find('image.png', '.');
+    });
+
     it('should return revved version if it ends hex in characters', function () {
       var rf = new RevvedFinder(function () {
         return ['11916fba.image.png'];
@@ -71,7 +80,7 @@ describe('RevvedFinder', function () {
 
     it('should restrict to the furnished subdirectories', function () {
       var rf = new RevvedFinder(function (pattern) {
-        assert.equal(pattern, '{temp,dist}/**/*fred.html');
+        assert.equal(pattern, '{temp,dist}/**/*fred\\.html');
         return ['fred.html'];
       }, ['temp', 'dist']);
       rf.find('fred.html', '.');
